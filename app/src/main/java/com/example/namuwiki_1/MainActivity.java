f1e949b9630bef33a8d9cba0131fff65459f77b0
joyandjay <chanjong.lee@outlook.com>
2021-12-26 14:33:35 +0900

    Original version at 2021-12-26

---
+++ app/src/main/java/com/example/namuwiki_1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.namuwiki_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+    private WebSettings mWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //웹뷰 시작
+        mWebView = findViewById(R.id.Namu_WebView);
+
+        mWebView.setWebViewClient(new WebViewClient());//클릭시 새창 안뜨게 함
+        mWebSettings = mWebView.getSettings();//세부 세팅 등록
+        mWebSettings.setJavaScriptEnabled(true);//웹페이지 자바스크립스 허용 여부
+        mWebSettings.setSupportMultipleWindows(false);//새창 띄우기 허용 여부
+        mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false);//자바스크립트 새창 띄우기 허용 여부 (멀티뷰)
+        mWebSettings.setLoadWithOverviewMode(true);//메타태그 허용 여부
+        mWebSettings.setUseWideViewPort(true);//화면 사이즈 맞추기 허용 여부
+        mWebSettings.setSupportZoom(false);//화면 줌 허용 여부
+        mWebSettings.setBuiltInZoomControls(false);//화면 확대 축소 혀용 여부
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);//컨텐츠 사이즈 맞추기
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);//브라우저 캐시 허용 여부
+        mWebSettings.setDomStorageEnabled(true);//로컬저장소 허용 여부
+
+        mWebView.loadUrl("https://namu.wiki");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()){
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
+
+
